565fa5c348a6c20d2076d8b446096e2072b58fe1
Jairo Castro <69816188+jairotgn@users.noreply.github.com>
2023-04-09 21:51:21 +0200

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token =  $user->createToken($user->id)->plainTextToken;
+
+        return response()->json([
+            'user' => $user->name,
+            'email' => $user->email,
+            'token' => $token
+        ],200);
+    }
+}
